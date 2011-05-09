945a400cd93d246ba655d9abe20caa0e3683d1df
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2011-05-09 12:19:38 -0500

    ready

--- launch/mvn-release.sh
+++ launch/mvn-release.sh
@@ -1,9 +1,14 @@
 
+rm release.properties pom.xml.releaseBackup
+
 git commit --all --message "ready"
 
 mvn release:prepare --batch-mode  --show-version --update-snapshots --define skipTests
 
 mvn release:perform --batch-mode  --show-version --update-snapshots --define skipTests
+
+mvn release:rollback --batch-mode  --show-version --update-snapshots --define skipTests
+
 
 
 
