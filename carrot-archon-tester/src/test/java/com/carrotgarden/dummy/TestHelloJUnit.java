db30d0d2568b49cd819a120fafa4ceb397c0589c
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2012-02-20 09:14:31 -0600

    a

---
+++ carrot-archon-tester/src/test/java/com/carrotgarden/dummy/TestHelloJUnit.java
@@ -0,0 +1,18 @@
+package com.carrotgarden.dummy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestHelloJUnit {
+
+	@Test
+	public void testMethod() {
+
+		System.out.println("*** hello junit ***");
+
+		assertTrue(true);
+
+	}
+
+}
---
+++ carrot-archon-tester/src/test/java/com/carrotgarden/dummy/TestHelloTestNG.java
@@ -0,0 +1,14 @@
+package com.carrotgarden.dummy;
+
+import org.testng.annotations.Test;
+
+public class TestHelloTestNG {
+
+	@Test
+	public void testMethod() {
+
+		System.out.println("*** hello testng ***");
+
+	}
+
+}
