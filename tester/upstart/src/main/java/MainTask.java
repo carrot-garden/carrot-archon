cf673326826e47ea0e20f222c033d44a4ed3ea20
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2014-04-28 18:24:55 -0500

    a

---
+++ tester/upstart/src/main/java/MainTask.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+
+public class MainTask {
+
+	public static void main(final String[] args) throws Exception {
+		
+		while(true){
+			System.out.println("" + new Date());
+			Thread.sleep(1000);
+		}
+
+	}
+	
+}
